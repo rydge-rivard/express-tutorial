df19d8b1cc15f7b2e0b4adeb64bed6a7243df4aa
Rydge <96926831+rydge-rivard@users.noreply.github.com>
2024-01-08 13:29:25 -0500

    Add Mongoose and MongoDB to app for db save, update, create, and delete.

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ const usersRouter = require("./routes/users");
 
 const app = express();
 
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB =
+  "mongodb+srv://myAtlasDBUser:dbpassword@cluster0.lz91hw2.mongodb.net/local_library?retryWrites=true&w=majority";
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+  next();
+});
+
+router.get("/cool", function (req, res, next) {
+  res.send("cool website");
 });
 
 module.exports = router;
